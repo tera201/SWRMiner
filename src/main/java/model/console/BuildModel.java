90adc3dd5e452464f156edcde26931355f54ee39
rnaryshkin <r.naryshkin99@gmail.com>
2025-03-20 16:27:16 +0300

    Refactoring

--- src/main/java/model/console/BuildModel.java
+++ src/main/java/model/console/BuildModel.java
@@ -3,9 +3,6 @@ package model.console;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Ref;
-import org.repodriller.RepositoryMining;
-import org.repodriller.filter.range.Commits;
-import org.repodriller.persistence.csv.CSVFile;
 import org.repodriller.scm.GitRemoteRepository;
 import org.repodriller.scm.GitRepository;
 import org.repodriller.scm.SCMRepository;
@@ -18,16 +15,13 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-
-import static java.util.Comparator.comparingInt;
 
 public class BuildModel {
 
   private static Logger log = LoggerFactory.getLogger(GitRepository.class);
 
-  public static void main(String[] args) throws CheckoutException, GitAPIException, IOException {
+  public static void main(String[] args) throws GitAPIException, IOException {
 
     String projectRoot = new File(".").getAbsolutePath();
     String csvPath = projectRoot.replace(".", "csv-generated");
@@ -118,29 +112,6 @@ public class BuildModel {
     repo.getScm().checkoutTo(branch);
   }
 
-  public void collectAuthorChanges(SCMRepository remoteGitRepo) {
-    String csvPath = System.getProperty("user.dir") + "/analyseGit";
-    FileCreatorVisitor fileCreateVisitor = new FileCreatorVisitor();
-    new RepositoryMining()
-            .in(remoteGitRepo)
-            .visitorsAreThreadSafe(true)
-            .withThreads(6)
-            .through(Commits.all())
-            .visitorsChangeRepoState(false)
-            .process(fileCreateVisitor,
-                    new CSVFile(csvPath + "/author-files-created.csv"))
-            .mine();
-
-    CSVFile csvByName = new CSVFile(
-            csvPath + "/author-files-created-count.csv",
-            new String[]{"Developer", "create-files"});
-
-    fileCreateVisitor.fileCreatorMap.entrySet()
-            .stream()
-            .sorted(comparingInt(entry -> entry.getValue().size()))
-            .forEach(e -> csvByName.write(e.getKey(), e.getValue().size()));
-  }
-
   public void cleanData() {
     String csvPath = System.getProperty("user.dir") + "/analyseGit";
     String gitPath = System.getProperty("user.dir") + "/clonedGit";
@@ -161,32 +132,5 @@ public class BuildModel {
     } catch (IOException e) {
       log.info("Delete failed: " + e);
     }
-  }
-
-  private static void collectAuthorChanges(String csvPath, SCMRepository remoteGitRepo) {
-    ModificationsVisitor visitor = new ModificationsVisitor();
-
-    new RepositoryMining()
-            .in(remoteGitRepo)
-            .through(Commits.all())
-            .process(visitor)
-            .mine();
-
-    // Write to file a list of pairs: <author, number-of-changes>
-    // sorted by author names.
-    CSVFile csvByName = new CSVFile(
-            csvPath + "/junit4-authors-by-name.csv",
-            new String[]{"developer", "changes"});
-    visitor.devs.forEach((developer, count) -> csvByName.write(developer, count));
-
-    // Write to file a list of pairs: <author, number-of-changes>
-    // sorted by the number of changes.
-    CSVFile csvByChanges = new CSVFile(
-            csvPath + "/junit4-by-changes.csv",
-            new String[]{"developer", "changes"});
-    visitor.devs.entrySet()
-            .stream()
-            .sorted(Map.Entry.comparingByValue())
-            .forEach(e -> csvByChanges.write(e.getKey(), e.getValue()));
   }
 }
